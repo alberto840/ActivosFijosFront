7d4eae3041e44882955f7bdc23b2a3ab8f2723f0
valdiux06 <juan.vada@ucb.edu.bo>
2023-09-26 19:47:49 -1100

    Login y custodio

--- frontendActivosFijos/src/app/RegistroDetallesActivo/registro-custodio/registro-custodio.component.ts
+++ frontendActivosFijos/src/app/RegistroDetallesActivo/registro-custodio/registro-custodio.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-custodio.component.css']
 })
 export class RegistroCustodioComponent {
+  custodio = {
+    nombre: '',
+    apellidoPaterno: '',
+    apellidoMaterno: '',
+    correo: '',
+    estado: 'activo'
+  };
 
+  onSubmit() {
+    // Aquí puedes manejar la lógica para enviar el formulario
+    console.log(this.custodio);
+  }
 }
